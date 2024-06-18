fa2d55bb98d7940aeef068ecd6fbabd601afe10c
DyNeX0 <offcthakur@gmail.com>
2024-06-18 00:57:10 -0600

    Added Update,Delete dynamic capabilities.

---
+++ app/profile/page.jsx
@@ -0,0 +1,58 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+import Profile from "@components/profile";
+
+const MyProfile = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Do you want to delete this prompt. Proceed if you are 100% sure!"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  return (
+    <Profile
+      name={`${session?.user.name.split(" ")[0]}'s`}
+      desc="Welcome to your personalized page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default MyProfile;
